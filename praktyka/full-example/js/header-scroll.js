38e91afaf8dfa7f5945717ffe93462666129baac
Adam S <adam@sweetanalytics.com>
2023-02-07 21:09:58 +0100

    praktyka

---
+++ praktyka/full-example/js/header-scroll.js
@@ -0,0 +1,13 @@
+window.onscroll = () => changeHeaderBackground();
+
+function changeHeaderBackground() {
+  const header = document.querySelector('[data-header]');
+  const headerOffsetTrigger = header.offsetHeight;
+  const pageOffset = window.pageYOffset;
+
+  if (pageOffset > headerOffsetTrigger) {
+    header.classList.add('header--transparent');
+  } else {
+    header.classList.remove('header--transparent');
+  }
+}
